095793fb11a0ef98edd6d64f724b23e49801e49e
skedwards88 <skedwards88@github.com>
2023-12-10 16:19:03 -0800

    update rule text

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,7 +5,7 @@ export default function Rules({setDisplay}) {
   return (
     <div className="App info">
       <h1>Wordfall: How to play</h1>
-      <p className="infoText">{`Wordfall is a meditative jumbled word search game.\n\nSwipe to connect letters into words. Each letter must be a horizontal, vertical, or diagonal neighbor of the previous letter.\n\nClear all letters of a color to reach the next level.`}</p>
+      <p className="infoText">{`Wordfall is a meditative jumbled word search game.\n\nSwipe to connect letters into words. Each letter must be a horizontal, vertical, or diagonal neighbor of the previous letter.\n\nClear all letters of a color to reach the next level.\n\nEarn bonuses by clearing levels and forming long words.`}</p>
       <button
         onClick={() => {
           setDisplay("game");
